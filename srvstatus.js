cf1d7c65547bbe03a7175cb1afc70c212f84c7cd
issa4700 <issa4700@gmail.com>
2019-11-29 16:46:53 +0000

    Fixed not being able to display players online

--- srvstatus.js
+++ srvstatus.js
@@ -3,6 +3,14 @@ module.exports = function(ip,port) {
     const status = server.all(ip,port);
 
     this.online = server.online(ip,port);
-    this.players = status.players.online;
-    this.max = status.players.max;
+    function players() {
+    	var player_list = server.list(ip,port);
+    	if (player_list.error) {
+    		return 0;
+    	} else {
+    		return player_list;
+    	}
+    }
+    this.players = players();
+    this.max = server.max_players(ip,port);
 }
